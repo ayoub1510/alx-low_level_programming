1c18200b7727eeae672a2c3631178b97c1d78dcc
ayoub1510 <xyoub77@gmail.com>
2023-10-08 08:28:46 -0700

    perfect

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string, followed by a newline
- * @s: string to print
+ * _print_rev_recursion - puts a string in reverse
+ * @s: char
+ * Return: -
  */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-	if (!*s)
+	if (*s != '\0')
 	{
-		_putchar('\n');
-		return;
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
-}
+
