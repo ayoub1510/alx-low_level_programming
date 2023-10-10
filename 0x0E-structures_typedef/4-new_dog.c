84e070cf3dd80f941fe443e788989eb9aefdc5c0
ayoub1510 <xyoub77@gmail.com>
2023-10-10 16:20:03 -0700

    wooooof

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,74 @@
+#include "dog.h"
+#include <stdlib.h>
+
+
+/**
+ * _strlen - length ofstr
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+
+	return (i);
+}
+
+/**
+ * _strcpy - cpy
+ * @dest: destination
+ * @src: source
+ *
+ * Return: pointer to destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of old struct
+ * @age: age
+ * @owner: owner
+ * Return:struct
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dogcpy;
+
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+	dogcpy = malloc(sizeof(dog_t));
+	if (dogcpy == NULL)
+		return (NULL);
+	(*dogcpy).name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if ((*dogcpy).name == NULL)
+	{
+		free(dogcpy);
+		return (NULL);
+	}
+	(*dogcpy).owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if ((*dogcpy).owner == NULL)
+	{
+		free((*dogcpy).name);
+		free(dogcpy);
+		return (NULL);
+	}
+	
+
+	(*dogcpy).name = _strcpy(dogcpy->name, name);
+	(*dogcpy).owner = _strcpy(dogcpy->owner, owner);
+	(*dogcpy).age = age;
+
+	return (dogcpy);
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+
+
+/**
+ * free_dog - free thedoggy
+ * @d: pointer to dog
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
